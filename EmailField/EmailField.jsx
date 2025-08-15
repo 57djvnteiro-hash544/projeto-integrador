2d8ca9db94e74ad188cb2cbdad4c97966f169a39
Heitor Quental <heitorquental321@gmail.com>
2025-08-15 15:36:35 -0300

    babababa
    fadsfafas

---
+++ EmailField/EmailField.jsx
@@ -0,0 +1,13 @@
+
+import EmailInput from "../EmailInput/EmailInput";
+
+
+function EmailField(){
+    return (
+        <div>
+        <label>CPF</label>
+        <EmailInput/>
+        </div>
+    );
+}
+export default EmailField
