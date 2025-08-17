889ecc18fd6010962b59e52a2f2c2ed027c35cad
Heitor Quental <heitorquental321@gmail.com>
2025-08-17 17:13:56 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { requestFormReset } from 'react-dom'
 
- import Saudacao from './Components/Saudacao/Saudacao.jsx'
- import Rodape from './Components/Rodape/Rodape.jsx'
- import Alerta from './Components/Alerta/Alerta.jsx'
- import CPFField from './Components/Email/CPFField.jsx'
- import LoginButton from './Components/Button/Button.jsx'
- import PasswordField from './Components/Password/PasswordField.jsx'
+import LoginForm from "./Components/Loginform/LoginForm.jsx"
  function App(){
   return (
-    <div> Olá Daniel
-      <Saudacao />
-      <Rodape />
-      <Alerta />
-      <CPFField />
-      <PasswordField />
-      <LoginButton />
+    <div>
+     <LoginForm />
     </div>
   )
  }
